cbe69e4a1225c0d71bd5ab48051424570ce30363
Seline <74591087+SelinevanDijke@users.noreply.github.com>
2021-06-15 11:31:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age == 18) {
+    console.log("Je bent oud genoeg, je mag naar binnen!");
+} else {
+    console.log("Je bent te jong, je mag niet naar binnen!");
+}
+
+if (isFemale == true) {
+    console.log("Je bent een vrouw en mag naar binnen.");
+} else {
+    console.log("Je bent geen vrouw en mag niet naar binnen.");
+}
+
+if (driverStatus == "bob") {
+    console.log("Je bent bob en mag naar huis toe rijden.");
+} else {
+    console.log("Je bent geen bob en moet een taxi bellen.");
+}
